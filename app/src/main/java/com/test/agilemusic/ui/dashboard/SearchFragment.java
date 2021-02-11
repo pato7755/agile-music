428e329b7d2fdd407831c221ed76968846ff5c24
Patrick Essiam <pat.essiam701@gmail.com>
2021-02-11 11:35:48 +0000

    Add SearchView and RecyclerView to searchfragment.xml

--- app/src/main/java/com/test/agilemusic/ui/dashboard/SearchFragment.java
+++ app/src/main/java/com/test/agilemusic/ui/dashboard/SearchFragment.java
@@ -4,26 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.test.agilemusic.R;
 
 public class SearchFragment extends Fragment {
 
-    private DashboardViewModel searchViewModel;
+    private SearchViewModel searchViewModel;
+    private SearchView searchView;
+    private RecyclerView recyclerView;
+    TextView textView;
+    View root;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        searchViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+
+        root = inflater.inflate(R.layout.fragment_search, container, false);
+
+        initViews();
+
         searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -32,4 +42,16 @@ public class SearchFragment extends Fragment {
         });
         return root;
     }
+
+
+    private void initViews(){
+
+        searchView = root.findViewById(R.id.searchview);
+        recyclerView = root.findViewById(R.id.recyclerview);
+        textView = root.findViewById(R.id.text_dashboard);
+
+
+
+    }
+
 }
